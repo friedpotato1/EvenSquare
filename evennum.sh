0be7a579784072d5802a04451de210b426c8b932
friedpotato1 <126858368+friedpotato1@users.noreply.github.com>
2023-03-17 23:24:17 +1100

    Update evennum.sh

--- evennum.sh
+++ evennum.sh
@@ -3,7 +3,7 @@ echo -e " Enter Number  : \c "
 read number
 ans=`echo " $number % 2 " | bc `
 if [[ $ans = "0" ]] ; then
-  ans1=`echo "sqrt ($number)" | bc `
+  ans1=`echo "scale=2;sqrt ($number)" | bc -l `
   echo " SquareRoot Is :- $ans1 "
 else
    echo " Number Isn't An Even Number "
